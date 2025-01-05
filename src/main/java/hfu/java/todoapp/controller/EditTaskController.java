10dda4cfeab48d8fd1fd816ee41f3c95cc844f41
thedmo <17425466+thedmo@users.noreply.github.com>
2025-01-05 16:02:47 +0100

    adds adding and editing tasks

---
+++ src/main/java/hfu/java/todoapp/controller/EditTaskController.java
@@ -0,0 +1,64 @@
+package hfu.java.todoapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import hfu.java.todoapp.common.models.TodoModel;
+import hfu.java.todoapp.components.services.CategoryService;
+import hfu.java.todoapp.components.services.TodoService;
+import hfu.java.todoapp.components.services.AiCategoryService;
+
+@Controller
+public class EditTaskController {
+
+    private TodoService todoService;
+    private CategoryService categoryService;
+    private AiCategoryService aiCategoryService;
+
+    @Autowired
+    public EditTaskController(TodoService todoService, CategoryService categoryService,
+            AiCategoryService aiCategoryService) {
+        this.todoService = todoService;
+        this.categoryService = categoryService;
+        this.aiCategoryService = aiCategoryService;
+    }
+
+    @GetMapping("/todos/editTask")
+    public String showEditPage(@RequestParam("id") Integer id, Model model) {
+        TodoModel todo = todoService.getById(id);
+        model.addAttribute("todo", todo);
+
+        model.addAttribute("categories", categoryService.getAll());
+        return "editTask";
+    }
+
+    @PostMapping("/todos/updateTask")
+    public String saveTask(
+            @ModelAttribute TodoModel task,
+            @RequestParam("categoryId") Integer categoryId,
+            @RequestParam("aiCategory") boolean aiCategory,
+            Model model) {
+
+        if (aiCategory) {
+            task.setCategory(aiCategoryService.requestCategory(task.getTask()));
+        } else if (categoryId != -1 && !aiCategory)
+            task.setCategory(categoryService.getById(categoryId));
+
+        todoService.save(task);
+        return "redirect:/todos/list";
+    }
+
+    @GetMapping("/todos/newTask")
+    public String showNewTaskPage(Model model) {
+        TodoModel todo = new TodoModel();
+        model.addAttribute("todo", todo);
+        model.addAttribute("categories", categoryService.getAll());
+        return "editTask";
+    }
+
+}
